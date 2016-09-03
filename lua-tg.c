6da093b8886e8ee875e43511e17cf4669b2cf886
clevermm <mm138111@gmail.com>
2016-09-03 21:46:45 +0430

    Update lua-tg.c

--- lua-tg.c
+++ lua-tg.c
@@ -660,7 +660,7 @@ void lua_chat_update (struct tgl_chat *C, unsigned flags) {
 //extern tgl_peer_t *Peers[];
 //extern int peer_num;
 
-#define MAX_LUA_COMMANDS 100000
+#define MAX_LUA_COMMANDS 1000
 
 struct lua_arg {
   int flags;
@@ -718,7 +718,6 @@ enum lua_query_type {
   lq_create_group_chat,
   lq_send_audio,
   lq_send_document,
-  lq_send_document2,
   lq_send_file,
   lq_load_audio,
   lq_load_document,
@@ -754,12 +753,6 @@ enum lua_query_type {
   lq_channel_set_username,
   lq_channel_set_admin,
   lq_channel_set_mod,
-  lq_reply_location,
-  lq_reply_audio,
-  lq_reply_document,
-  lq_reply_file,
-  lq_reply_photo,
-  lq_reply_video,
   lq_channel_demote
 };
 
@@ -1270,10 +1263,6 @@ void lua_do_all (void) {
       tgl_do_send_document (TLS, lua_ptr[p + 1].peer_id, lua_ptr[p + 2].str, NULL, 0, 0, lua_msg_cb, lua_ptr[p].ptr);
       p += 3;
       break;
-	case lq_send_document2:
-		tgl_do_send_document(TLS, lua_ptr[p + 1].peer_id, lua_ptr[p + 2].str, lua_ptr[p + 3].str, strlen(lua_ptr[p + 3].str), 0, lua_msg_cb, lua_ptr[p].ptr);
-		p += 3;
-		break;
     case lq_send_file:
       tgl_do_send_document (TLS, lua_ptr[p + 1].peer_id, lua_ptr[p + 2].str, NULL, 0, TGL_SEND_MSG_FLAG_DOCUMENT_AUTO, lua_msg_cb, lua_ptr[p].ptr);
       p += 3;
@@ -1286,31 +1275,6 @@ void lua_do_all (void) {
       tgl_do_set_chat_photo (TLS, lua_ptr[p + 1].peer_id, lua_ptr[p + 2].str, lua_empty_cb, lua_ptr[p].ptr);
       p += 3;
       break;
-	case lq_reply_audio:
-		tgl_do_reply_document(TLS, &lua_ptr[p + 1].msg_id, lua_ptr[p + 2].str, NULL, 0, TGL_SEND_MSG_FLAG_DOCUMENT_AUDIO, lua_msg_cb, lua_ptr[p].ptr);
-		p += 3;
-		break;
-	case lq_reply_document:
-		tgl_do_reply_document(TLS, &lua_ptr[p + 1].msg_id, lua_ptr[p + 2].str, NULL, 0, 0, lua_msg_cb, lua_ptr[p].ptr);
-		p += 3;
-		break;
-	case lq_reply_file:
-		tgl_do_reply_document(TLS, &lua_ptr[p + 1].msg_id, lua_ptr[p + 2].str, NULL, 0, TGL_SEND_MSG_FLAG_DOCUMENT_AUTO, lua_msg_cb, lua_ptr[p].ptr);
-		p += 3;
-		break;
-	case lq_reply_location: // TODO - I DON'T UNDERSTAND WHY IT'S NOT WORKING
-		tgl_do_reply_location(TLS, &lua_ptr[p + 1].msg_id, lua_ptr[p + 2].dnum, lua_ptr[p + 3].dnum, 0, lua_msg_cb, lua_ptr[p].ptr);
-		p += 4;
-		break;
-	case lq_reply_photo:
-		tgl_do_reply_document(TLS, &lua_ptr[p + 1].msg_id, lua_ptr[p + 2].str, NULL, 0, TGL_SEND_MSG_FLAG_DOCUMENT_PHOTO, lua_msg_cb, lua_ptr[p].ptr);
-		p += 3;
-		break;
-	case lq_reply_video:
-		tgl_do_reply_document(TLS, &lua_ptr[p + 1].msg_id, lua_ptr[p + 2].str, NULL, 0, TGL_SEND_MSG_FLAG_DOCUMENT_VIDEO, lua_msg_cb, lua_ptr[p].ptr);
-		p += 3;
-		break;
-
     case lq_load_photo:
     case lq_load_video:
     case lq_load_audio:
@@ -1616,7 +1580,6 @@ struct lua_function functions[] = {
   {"send_video", lq_send_video, { lfp_peer, lfp_string, lfp_none }},
   {"send_audio", lq_send_audio, { lfp_peer, lfp_string, lfp_none }},
   {"send_document", lq_send_document, { lfp_peer, lfp_string, lfp_none }},
-  { "send_document2", lq_send_document, { lfp_peer, lfp_string, lfp_string, lfp_none } },
   {"send_file", lq_send_file, { lfp_peer, lfp_string, lfp_none }},
   {"send_text", lq_send_text, { lfp_peer, lfp_string, lfp_none }},
   {"chat_set_photo", lq_chat_set_photo, { lfp_chat, lfp_string, lfp_none }},
@@ -1679,14 +1642,6 @@ struct lua_function functions[] = {
   {"channel_set_admin", lq_channel_set_admin, { lfp_channel, lfp_peer, lfp_none }},
   {"channel_set_mod", lq_channel_set_mod, { lfp_channel, lfp_peer, lfp_none }},
   {"channel_demote", lq_channel_demote, { lfp_channel, lfp_peer, lfp_none }},
-  { "reply_msg", lq_reply, { lfp_msg, lfp_string, lfp_none } },
-  { "reply_file", lq_reply_file, { lfp_msg, lfp_string, lfp_none } },
-  { "reply_audio", lq_send_audio, { lfp_msg, lfp_string, lfp_none } },
-  { "reply_location", lq_reply_location, { lfp_msg, lfp_double, lfp_double, lfp_none } },
-  { "reply_document", lq_reply_document, { lfp_msg, lfp_string, lfp_none } },
-  { "reply_photo", lq_reply_photo, { lfp_msg, lfp_string, lfp_none } },
-  { "reply_video", lq_reply_video, { lfp_msg, lfp_string, lfp_none } },
-
   { 0, 0, { lfp_none}}
 };
 
